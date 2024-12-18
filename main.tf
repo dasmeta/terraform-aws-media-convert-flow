130ae4bf58a452b14168d9a88025c6e29687cf6c
Sarhad <sarhad@dasmeta.com>
2024-12-18 19:24:41 +0400

    init commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_media_convert_queue" "queue" {
+  name = var.queue_name
+  pricing_plan = var.queue_pricing_plan
+
+  status = var.status
+
+}
+
+module "iam_role" {
+  source = "dasmeta/iam/aws//modules/role"
+  version = "1.2.1"
+
+  name        = "MediaConverter_default_role"
+  description = "Role to be attached to MediaConverter jobs"
+
+  policy = [
+    {
+      actions   = ["s3:*", "s3-object-lambda:*"]
+      resources = ["*"]
+    }
+  ]
+
+  trust_relationship = [
+    {
+      principals = {
+        type        = "Service"
+        identifiers = ["mediaconvert.amazonaws.com"]
+      },
+      actions = ["sts:AssumeRole"]
+    }
+  ]
+
+}
